cd3499c16a8bcd0adee94422b13cf8aaee1a7904
bogdanovdenis95 <156674464+bogdanovdenis95@users.noreply.github.com>
2024-08-30 19:59:48 +0300

    Update tasks.py

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,19 +1,20 @@
 import asyncio
-
 import telegram
 from celery import shared_task
-
+from decouple import config
 from .models import Habit
 
 
 @shared_task
 def send_telegram_notification(habit_id):
     habit = Habit.objects.get(id=habit_id)
-    bot = telegram.Bot(token='6875683912:AAHydQ3a5Sm8Y2oSvvMFeEJCVlEodQI7-X0')
+    bot_token = config('TELEGRAM_BOT_TOKEN')
+    chats_id = config('TELEGRAM_CHAT_ID')
+    bot = telegram.Bot(token=bot_token)
 
     async def send_message():
         message = f"Напоминание: {habit.action} в {habit.time} в {habit.location}."
-        await bot.send_message(chat_id='715858633', text=message)
+        await bot.send_message(chat_id=chats_id, text=message)
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(send_message())
